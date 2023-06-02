ded37468e51023c82085cbd09fde33caf149b1d5
tobi <tobibags19@gmail.com>
2023-06-02 05:59:06 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,15 @@
+import justpy as jp
+
+
+class Api:
+    """Handles requests at /api?w=word"""
+    @classmethod
+    def serve(cls, req):
+        wp = jp.WebPage()
+        word = req.query_params.get('w')
+        jp.Div(a=wp, text=word.title())
+        return wp
+
+
+jp.Route("/api", Api.serve)
+jp.justpy()
